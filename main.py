a7fd15574170959f0c4ecefeec3c77ca1538f246
ChrisCas92 <chriscas.dev@gmail.com>
2025-05-27 17:51:02 +0200

    Added a rotation Method to rotat left and right, also added gameclock to manage the framerate

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from player import Player
 def main():
     pygame.init()  # Initialize all imported pygame modules
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create the game window
+    clock = pygame.time.Clock()  # Create a clock to manage frame rate
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.update(clock.tick() / 1000.0)  # Update player state based on time elapsed
         player.draw(screen)  # Draw the player on the screen
         pygame.display.flip()  # Update the display
     
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 # Define colors
 white = (255, 255, 255)
@@ -22,3 +22,16 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, white, self.triangle(), 2)
         # Update player position based on velocity
+        
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # Rotate left
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            # Rotate right
+            self.rotate(dt)
