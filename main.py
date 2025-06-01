c8c277b0a5732ef2d4e4620183b468bb3e27e089
ChrisCas92 <chriscas.dev@gmail.com>
2025-06-01 16:24:33 +0200

    added all instances of Player to groups for better management

--- main.py
+++ main.py
@@ -10,19 +10,35 @@ def main():
     pygame.init()  # Initialize all imported pygame modules
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create the game window
     clock = pygame.time.Clock()  # Create a clock to manage frame rate
+    
+    updatable = pygame.sprite.Group()  # Create a group for updateable objects
+    drawable = pygame.sprite.Group()  # Create a group for drawable objects
+    
+    Player.containers = (updatable, drawable)  # Set the containers for the Player class
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)  # Create a player instance at the center of the screen
+    
+    dt = 0  # Initialize delta time variable
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
-        player.update(clock.tick() / 1000.0)  # Update player state based on time elapsed
-        player.draw(screen)  # Draw the player on the screen
+
+        updatable.update(dt)
+        
+        screen.fill("black")  # Clear the screen with black color)
+        
+        for obj in drawable:
+            obj.draw(screen)  # Draw all drawable objects on the screen
+        
+        
         pygame.display.flip()  # Update the display
     
-player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)  # Create a player instance at the center of the screen
-
+        # limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000.0  # Update player state based on time elapsed
+        
     
-
 
 if __name__ == "__main__":
     main()
